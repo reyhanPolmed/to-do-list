7c27db6b85e92bfdf5f411858defadf7bf89d3de
reyhan <jhonsonrahmat@gmail.com>
2026-03-11 00:28:08 +0700

    pembaruan

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,11 @@
 import { PrismaClient } from "@prisma/client"
-import { PrismaPg } from "@prisma/adapter-pg"
-import pg from "pg"
 
-const pool = new pg.Pool({
-  connectionString: process.env.DATABASE_URL
-})
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
 
-const adapter = new PrismaPg(pool)
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
-const prisma = new PrismaClient({
-  adapter
-})
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
 
-export default prisma
+export default prisma
